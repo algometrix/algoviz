e16574e8ae11d52871262f569a2aecf8c4de0725
Ashish Nagar <ashishnagar31@gmail.com>
2021-03-08 09:15:53 -0500

    Count Palindrome Demo

---
+++ Demos/count_palin.py
@@ -0,0 +1,18 @@
+from vizlist import VizList
+
+def countSubstrings(s):
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    dp = VizList(dp, column_index=list(s),row_index=list(s), title_name='DP Table')
+    res = 0
+    for i in range(n - 1, -1, -1):
+        for j in range(i, n):
+            dp[i][j] = s[i] == s[j] and ((j - i + 1) < 3 or dp[i + 1][j - 1])
+            res += dp[i][j]
+    return res
+
+
+if __name__ == '__main__':
+    s = 'aabac'
+    res = countSubstrings(s)
+    print('Output : {}'.format(res))
