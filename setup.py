adbac3feb6c476fdfa2c749b56fe8e001564886f
Ashish Nagar <ashishnagar31@gmail.com>
2021-03-09 15:51:23 -0500

    Updating release url

--- setup.py
+++ setup.py
@@ -7,21 +7,18 @@ setup(
   description = 'Generates visualizations for arrays',   # Give a short description about your library
   author = 'Ashish Nagar',                   # Type in your name
   author_email = 'ashishnagar31@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url = 'https://github.com/algometrix/algoviz',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/algometrix/algoviz/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['Visualization', 'Algorithms'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'rich',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
